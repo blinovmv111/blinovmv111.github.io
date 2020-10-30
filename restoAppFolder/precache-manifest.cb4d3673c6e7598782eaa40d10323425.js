298f054440441fb8d2a34605dafd8d63a664d8ed
blinovmv111 <72444552+blinovmv111@users.noreply.github.com>
2020-10-30 11:24:26 +0300

    Add files via upload

---
+++ restoAppFolder/precache-manifest.cb4d3673c6e7598782eaa40d10323425.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce46eb4820dc15bd16b6e5bb0590c3e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4995818b8c62b56960ad",
+    "url": "/static/css/main.7e248db8.chunk.css"
+  },
+  {
+    "revision": "ea76cdb31076b31e289a",
+    "url": "/static/js/2.fe036df5.chunk.js"
+  },
+  {
+    "revision": "4995818b8c62b56960ad",
+    "url": "/static/js/main.660acfc9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "698e5e807daf0026f6ac590ce288822c",
+    "url": "/static/media/food-bg.698e5e80.jpg"
+  },
+  {
+    "revision": "17087a96763ac04caa08db981aef53ba",
+    "url": "/static/media/meat.17087a96.svg"
+  },
+  {
+    "revision": "dc87f2e6b90e66558b321b8963840631",
+    "url": "/static/media/pizza.dc87f2e6.svg"
+  },
+  {
+    "revision": "f44865448ebe64a2dadcafadf7317f74",
+    "url": "/static/media/salad.f4486544.svg"
+  },
+  {
+    "revision": "701d6d1d57f5d528c4db1e887ff29e82",
+    "url": "/static/media/shopping-cart-solid.701d6d1d.svg"
+  }
+]);
